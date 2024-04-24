8ed51e3b36b877ad207182a24bf291d8ab103dbe
Gabriel Monteiro Fernandes <mfgabriel92@gmail.com>
2024-04-24 21:50:34 +0800

    feat(Basket): global usings

--- Services/Basket.API/GlobalUsings.cs
+++ Services/Basket.API/GlobalUsings.cs
@@ -1,6 +1,10 @@
 global using Carter;
 global using MediatR;
 global using Mapster;
+global using Marten;
 global using FluentValidation;
 global using Basket.API.Models;
+global using Basket.API.Setup;
 global using BuildingBlocks.CQRS;
+global using BuildingBlocks.Exceptions;
+global using BuildingBlocks.Behaviors;
