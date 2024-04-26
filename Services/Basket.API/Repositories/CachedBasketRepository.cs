ff7af988c7617c86b9e0538992ae3ae9231640a3
Gabriel Monteiro Fernandes <mfgabriel92@gmail.com>
2024-04-26 23:45:46 +0800

    feat(Basket): added class for cached repository

---
+++ Services/Basket.API/Repositories/CachedBasketRepository.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
+
+namespace Basket.API.Repositories;
+
+public class CachedBasketRepository(IBasketRepository repository, IDistributedCache cache) : IBasketRepository
+{
+    public async Task<ShoppingCart> GetBasketAsync(string username)
+    {
+        var cached = await cache.GetStringAsync(username);
+
+        if (!string.IsNullOrEmpty(cached))
+        {
+            return JsonSerializer.Deserialize<ShoppingCart>(cached)!;
+        }
+
+        var shoppingCart = await repository.GetBasketAsync(username);
+        await cache.SetStringAsync(username, JsonSerializer.Serialize(shoppingCart));
+
+        return shoppingCart;
+    }
+
+    public async Task<ShoppingCart> StoreBasketAsync(ShoppingCart shoppingCart)
+    {
+        await repository.StoreBasketAsync(shoppingCart);
+        await cache.SetStringAsync(shoppingCart.Username, JsonSerializer.Serialize(shoppingCart));
+        return shoppingCart;
+    }
+
+    public async Task DeleteBasketAsync(string username)
+    {
+        await repository.DeleteBasketAsync(username);
+        await cache.RemoveAsync(username);
+    }
+}
