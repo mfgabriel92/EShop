6d4c1945bbe059c2049b8b8f2a35c7ce86442ff7
Gabriel Monteiro Fernandes <mfgabriel92@gmail.com>
2024-04-30 09:12:27 +0800

    feat(Discount): added discount grpc project and Coupon model

---
+++ Services/Discount.gRPC/Models/Coupon.cs
@@ -0,0 +1,12 @@
+﻿namespace Discount.gRPC.Models;
+
+public class Coupon
+{
+    public Guid Id { get; set; }
+
+    public string ProductName { get; set; } = default!;
+
+    public string Description { get; set; } = default!;
+
+    public int Amount { get; set; }
+}
