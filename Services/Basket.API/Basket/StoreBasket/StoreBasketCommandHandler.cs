13769b483ed638f4e69e357e9baaf3e3455b8380
Gabriel Monteiro Fernandes <mfgabriel92@gmail.com>
2024-05-12 18:42:03 +0800

    feat(Basket): consume discount upon storing basket

--- Services/Basket.API/Basket/StoreBasket/StoreBasketCommandHandler.cs
+++ Services/Basket.API/Basket/StoreBasket/StoreBasketCommandHandler.cs
@@ -1,12 +1,30 @@
+using Discount.gRPC;
+
 namespace Basket.API.Basket.StoreBasket;
 
-public class StoreBasketCommandHandler(IBasketRepository repository)
+public class StoreBasketCommandHandler(IBasketRepository repository, DiscountProtoService.DiscountProtoServiceClient discountProto)
     : ICommandHandler<StoreBasketCommand, StoreBasketResult>
 {
     public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
     {
         var shoppingCart = command.ShoppingCart;
+
+        await ApplyDiscount(discountProto, shoppingCart.Items);
         await repository.StoreBasketAsync(shoppingCart);
+
         return new StoreBasketResult(shoppingCart);
+    }
+
+    private static async Task ApplyDiscount(DiscountProtoService.DiscountProtoServiceClient discountProto, List<ShoppingCartItem> items)
+    {
+        foreach (var item in items)
+        {
+            var coupon = await discountProto.GetDiscountAsync(new GetDiscountRequest
+            {
+                Id = item.Id
+            });
+
+            item.Price -= coupon.Amount;
+        }
     }
 }
--- Services/Basket.API/Models/ShoppingCartItem.cs
+++ Services/Basket.API/Models/ShoppingCartItem.cs
@@ -2,10 +2,10 @@ namespace Basket.API.Models;
 
 public class ShoppingCartItem
 {
-    public Guid Id { get; set; } = default!;
+    public int Id { get; set; } = default!;
 
     public string Name { get; set; } = default!;
-    
+
     public int Quantity { get; set; } = default!;
 
     public decimal Price { get; set; } = default!;
