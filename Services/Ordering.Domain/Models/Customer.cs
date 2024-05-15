37d0528d499cde1ff3a5792e188a7551f85b13ce
Gabriel Monteiro Fernandes <mfgabriel92@gmail.com>
2024-05-15 11:36:59 +0800

    feat(Ordering): Create method for rich-domain in DDD

--- Services/Ordering.Domain/Models/Customer.cs
+++ Services/Ordering.Domain/Models/Customer.cs
@@ -5,4 +5,17 @@ public class Customer : Entity<CustomerId>
     public string Name { get; private set; } = default!;
 
     public string Email { get; private set; } = default!;
+
+    public static Customer Create(string name, string email)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(name);
+        ArgumentException.ThrowIfNullOrWhiteSpace(email);
+
+        return new Customer
+        {
+            Id = CustomerId.Of(Guid.NewGuid()),
+            Name = name,
+            Email = email
+        };
+    }
 }
--- Services/Ordering.Domain/ValueObjects/CustomerId.cs
+++ Services/Ordering.Domain/ValueObjects/CustomerId.cs
@@ -2,5 +2,19 @@
 
 public record CustomerId
 {
+    private CustomerId(Guid value) => Value = value;
+
     public Guid Value { get; }
+
+    public static CustomerId Of(Guid value)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+
+        if (value == Guid.Empty)
+        {
+            throw new DomainException("The ID cannot be null");
+        }
+
+        return new CustomerId(value);
+    }
 }
